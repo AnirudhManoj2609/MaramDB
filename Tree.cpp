4641eeb71baac3adf900ca69ab8ab31d7665b524
AnirudhManoj2609 <anirudhmanoj2609@gmail.com>
2025-10-31 09:19:19 +0500

    created

--- Tree.cpp
+++ Tree.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <string>
 
 class Tree : public std::enable_shared_from_this<Tree>{
 
-    int count;
-    int child;
-    std::vector<std::shared_ptr<Tree>> children;
-public:
-    Tree(int cnt=0) : count(cnt){}
+    private:
+        int id;
+        int count;
+        std::string name;
+        std::vector<std::shared_ptr<Tree>> children;
+        static int global_id;
 
-    Tree(std::shared_ptr<Tree> tree,int cnt=0) : count(cnt){
-        if(tree){
-            tree->addChild(shared_from_this());
+        Tree(std::string name_,int cnt=0) : id(global_id++),name(name_), count(cnt){}
+    
+    public:
+        static std::shared_ptr<Tree> createRoot(std::string name_,int cnt=0){
+            return std::make_shared<Tree>(name_,cnt);
         }
-    }
-    void addChild(std::shared_ptr<Tree> child){
-        children.push_back(child);
-    }
+        
+        static std::shared_ptr<Tree> createChild(std::shared_ptr<Tree> parent,std::string name_,int cnt=0){
+            auto child = std::make_shared<Tree>(name_,cnt);
+            if(parent){
+                parent->addChild(child);
+            }
+            return child;
+        }
+        void addChild(std::shared_ptr<Tree> child){
+            children.push_back(child);
+        }
+
 };
